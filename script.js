fe2054e8758b1c1ac7c78ddba4415bd938f70995
Hans Trautlein <hans@trautlein.com>
2016-12-05 20:20:05 -0800

    Almost there with chapter 13, need to beautify labels

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-var dataset = [5, 10, 15, 20, 25];
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-var svg = d3.select('body').append('svg');
+// Width and height
+var w = 500;
+var h = 100;
+var barPadding = 1;
 
-var circles = svg.selectAll('circle')
-                 .data(dataset)
-                 .enter()
-                 .append('circle');
+var svg = d3.select('body')
+            .append('svg')
+            .attr('width', w)
+            .attr('height', h);
 
-circles.attr('cx', (d, i) => i * 50 + 25)
-       .attr('cy', 25)
-       .attr('r', d => d);
+svg.selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("x", (d, i) => i * (w / dataset.length))
+  .attr("y", d => h - (d * 4))
+  .attr("width", w / dataset.length - barPadding)
+  .attr("height", d => d * 4)
+  .attr('fill', d => "rgb(0, 0, " + (d * 10) + ")")
+  .text(d => d)
+
+svg.selectAll('text')
+  .data(dataset)
+  .enter()
+  .append('text')
+  .text(d => d)
+  .attr('x', (d, i) => {
+    return i * (w / dataset.length) + 5;
+  })
+  .attr('y', d => {
+    return h - (d * 4) + 15;
+  })
+  .attr('font-family', 'sans-serif')
+  .attr('font-size', '11px')
+  .attr('fill', 'white');
 
